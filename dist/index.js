25b6d9537ea6c423bbc13b1c32df32c12e130330
ChirilaIonut <chirila.ionut87@gmail.com>
2021-05-20 09:44:57 +0300

    updates

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var contract = require("./lib/amm-lib/contract");
 var helper = require("./lib/amm-lib/helper");
-var types = require("./lib/amm-lib/types");
-exports.default = { contract: contract, helper: helper, types: types };
+var amTypes = require("./lib/amm-lib/types");
+exports.default = { contract: contract, helper: helper, amTypes: amTypes };
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as contract from "./lib/amm-lib/contract"
 import * as helper from "./lib/amm-lib/helper"
-import * as types from "./lib/amm-lib/types"
+import * as amTypes from "./lib/amm-lib/types"
 
-export default { contract, helper, types }
+export default { contract, helper, amTypes }
